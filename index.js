d266172afffd0c58164d81cc969efcd9c053c086
shaineblack <shaineblck@gmail.com>
2024-01-22 14:40:01 -0500

    dom modifying lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const mainNode = document.querySelector('#main');
+mainNode.remove();
+
+const newHeader = document.createElement('h1');
+
+// Step 2: Set the id of the newHeader element to 'victory'
+newHeader.id = 'victory';
+
+// Step 3: Set the text content of the newHeader element
+newHeader.textContent = 'Shaine is the champion'; 
